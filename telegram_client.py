47b5594d1576fb33737db101c15f546d1f57ffbe
viktorbryanskiy1-crypto <viktor.bryanskiy1@gmail.com>
2025-08-18 02:06:37 +0800

    Update telegram_client.py

--- telegram_client.py
+++ telegram_client.py
@@ -1,23 +1,25 @@
 from telethon import TelegramClient
 
-# Вставь сюда свои данные
-api_id = 20307782       # число, выданное Telegram
-api_hash = '8408f037ff82f1cf2270ae0d41823fe4' # строка, выданная Telegram
-phone_number = '+7832438267'  # твой номер телефона с кодом страны
+api_id = 20307782
+api_hash = '8408f037ff82f1cf2270ae0d41823fe4'
+phone_number = '+7832438267'
 
-# Создаем клиент
 client = TelegramClient('session_name', api_id, api_hash)
 
 async def main():
     await client.start(phone=phone_number)
     print("✅ Клиент подключен!")
 
-    # Пример: получаем последние 5 сообщений из твоей группы
-    group_username = 'CinemaSpaceMyBot'  # @username группы
-    async for message in client.iter_messages(group_username, limit=5):
-        print(message.id, message.text, message.media)
+    group_username = 'kinofilmuni'  # Заменить на юзернейм канала
 
-# Запуск клиента
+    async for message in client.iter_messages(group_username, limit=5):
+        print(f"ID: {message.id}")
+        if message.text:
+            print(f"Текст: {message.text}")
+        if message.media:
+            print(f"Есть медиа: {message.media}")
+        print('---------------------')
+
 if __name__ == '__main__':
     import asyncio
     asyncio.run(main())
