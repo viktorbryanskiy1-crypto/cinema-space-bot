691508a4217260df7c723f0bc0ee875183490f59
viktorbryanskiy1-crypto <viktor.bryanskiy1@gmail.com>
2025-08-18 02:23:05 +0800

    Update telegram_client.py

--- telegram_client.py
+++ telegram_client.py
@@ -1,8 +1,8 @@
 from telethon import TelegramClient
 
-api_id = 20307782
-api_hash = '8408f037ff82f1cf2270ae0d41823fe4'
-phone_number = '+7832438267'
+api_id = 20307782       # твой api_id
+api_hash = '8408f037ff82f1cf2270ae0d41823fe4'  # твой api_hash
+phone_number = '+79832438267'  # твой номер телефона
 
 client = TelegramClient('session_name', api_id, api_hash)
 
@@ -10,15 +10,12 @@ async def main():
     await client.start(phone=phone_number)
     print("✅ Клиент подключен!")
 
-    # Используем get_entity для приватного или публичного канала
-    group = await client.get_entity('kinofilmuni')  # заменяй на ссылку или юзернейм
-    async for message in client.iter_messages(group, limit=5):
-        print(f"ID: {message.id}")
-        if message.text:
-            print(f"Текст: {message.text}")
-        if message.media:
-            print("Есть медиа!")
-        print('---------------------')
+    # Выводим все диалоги (чат, каналы, группы), в которых ты участвуешь
+    async for dialog in client.iter_dialogs():
+        if dialog.is_channel:
+            print(f"Название канала: {dialog.name}")
+            print(f"ID канала: {dialog.id}")
+            print(f"Username: {dialog.username}\n")
 
 if __name__ == '__main__':
     import asyncio
