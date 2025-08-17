d8294a58c6c8889143fa0b5282b24d6b20748823
viktorbryanskiy1-crypto <viktor.bryanskiy1@gmail.com>
2025-08-18 01:07:28 +0800

    Update db.py

--- db.py
+++ db.py
@@ -1,14 +1,12 @@
 import psycopg2
 from psycopg2.extras import RealDictCursor
 
-# Параметры базы Render
-DB_HOST = "dpg-d2dis5c9c44c73fa50q0-a"
-DB_NAME = "cinema-db"
+DB_HOST = "dpg-d2dis5c9c44c73fa50q0-a.oregon-postgres.render.com"
+DB_PORT = "5432"
+DB_NAME = "cinema_db_jvvx"
 DB_USER = "cinema_db_jvvx_user"
 DB_PASSWORD = "jh0QW70EhxsKHx3mEq4CqGRSRhua7F5n"
-DB_PORT = "5432"  # Обычно 5432
 
-# Подключение
 def get_connection():
     conn = psycopg2.connect(
         host=DB_HOST,
@@ -20,7 +18,6 @@ def get_connection():
     )
     return conn
 
-# Пример: получить все видео
 def get_all_videos():
     conn = get_connection()
     cur = conn.cursor()
@@ -30,7 +27,6 @@ def get_all_videos():
     conn.close()
     return videos
 
-# Пример вызова
 if __name__ == "__main__":
     videos = get_all_videos()
     print(videos)
